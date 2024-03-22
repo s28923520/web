9425eba2c8d2e584bf3dfc4a0b70750f55d988ec
s28923520 <s28923520@gmail.com>
2024-03-22 10:05:32 +0800

    創建資料表

---
+++ create.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>建立表單</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/func.js"></script>
+    <style>
+        button {
+            height: 40px;
+        }
+        button:hover {
+            background-color: #C7DDC1;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include "connect.php"; ?>
+
+    <form action="createfun.php" action="post" method="post">
+        <label for="group">新增團務表單：</label>
+        <input type="text" name="groupname" placeholder="團務名稱">
+        <br>
+        <button type="submit" name="creategroup">建立團務表單</button>
+    </form>
+    <br>
+    <form action="createfun.php" action="post" method="post">
+        <label for="claim">新增認領表單：</label>
+        <input type="text" name="claimname" placeholder="認領名稱">
+        <br>
+        <button type="submit" name="createclaim">建立認領表單</button>
+    </form>
+
+    <hr>
+    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" id="tableForm">
+        <select name="table" onchange="getTableData()">
+            <option value="" disabled selected>請選擇資料表</option>
+            <?php
+            $tables->data_seek(0);
+            while ($row = $tables->fetch_row()) {
+                $selected = ($_POST["table"] == $row[0]) ? "selected" : "";
+                echo "<option value='" . $row[0] . "'$selected>" . $row[0] . "</option>";
+            }
+            ?>
+        </select>
+    </form>
+
+
+    <div id="tableDataContainer"></div>
+</body>
